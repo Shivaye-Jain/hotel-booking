e01e6b65be9c7f8123646065cb7fcc069ceed818
Shivaye-Jain <jainshivaye25@gmail.com>
2025-07-23 12:24:44 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -5,23 +5,23 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebHooks from './controllers/clerkWebHooks.js';
 
-connectDB(); 
- 
+await connectDB();
+
 const app = express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 
 // Middleware
-app.use(express.json( ))
+app.use(express.json())
 app.use(clerkMiddleware());
 
 // API to listen to Clerk Webhooks
 app.use("/api/clerk", clerkWebHooks)
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("API is working")
 })
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
