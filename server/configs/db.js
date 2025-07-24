aa3bae5ed7eeb5e0fd8ba487159b7463fe6bcb82
Shivaye-Jain <jainshivaye25@gmail.com>
2025-07-24 10:33:54 +0530

    twelfth commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,23 +1,12 @@
 import mongoose from "mongoose";
 
-mongoose.connection.on("connected", () => {
-  console.log("✅ Database connected");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("❌ Database connection error:", err);
-});
-
-const connectDB = async () => {
-  try {
-    mongoose.set("strictQuery", true);
-
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "hotel-booking", // optional if already in URI
-    });
-  } catch (error) {
-    console.error("❌ DB connection failed:", error.message);
-  }
-};
+const connectDB = async () =>{
+    try{
+        mongoose.connection.on('connected', ()=> {console.log("Database Connected")})
+        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+    } catch(error){
+        console.log(error.message);
+    }
+}
 
 export default connectDB;
